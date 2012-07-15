1b4b66af769fb61c6b39d445b78268abdedf767a
ripan valentin <vripan@gmail.com>
2012-07-15 12:45:44 +0300

    Creation of projects feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Ticketee::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "projects#index"
+
+  resources :projects
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
