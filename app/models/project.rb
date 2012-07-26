26bde60e0060ef3c5528cafad8621e3d834b7c82
ripan valentin <vripan@gmail.com>
2012-07-26 23:02:40 +0300

    made projects only visible to users with permissions to see them

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,12 @@
 class Project < ActiveRecord::Base
   has_many :tickets, :dependent => :destroy
+  has_many :permissions, :as => :thing
 
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => true
   attr_accessible :name
+
+  scope :readable_by, lambda { |user|
+    joins(:permissions).where(:permissions => { :action => "view", :user_id => user.id })
+  }
+
 end
--- features/viewing_projects.feature
+++ features/viewing_projects.feature
@@ -11,7 +11,6 @@ I want to be able to see a list of available projects
     And "user@ticketee.com" can view the "TextMate 2" project
 
   Scenario: Listing all projects
-    Given there is a project called "TextMate 2"
     And I am on the homepage
     When I follow "TextMate 2"
     Then I should be on the project page for "TextMate 2"
