106fde4f10f353a75612d410eb2494e20c14f33e
lwiq <vripan@gmail.com>
2012-07-30 23:06:36 +0300

    added creating, editing, deleting tickets to assigning permissions interface

--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,13 @@ Ticketee::Application.routes.draw do
   # match ':controller(/:action(/:id))(.:format)'
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
+
+  put '/admin/users/:user_id/permissions',
+    :to => 'admin/permissions#update',
+    :as => :update_user_permissions
+
 end
